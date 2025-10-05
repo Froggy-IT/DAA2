3bb1d2370190d5e852bf54c2ef595b7a83b93083
Froggy-IT <kalzhanovzhansultan@gmail.com>
2025-10-05 14:52:26 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ import metrics.PerformanceTracker;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90, 42, 5};
-        ShellSort shellSort = new ShellSort(GapSequence.KNUTH);
-        PerformanceTracker tracker = shellSort.getTracker();
-        tracker.start();
-        shellSort.sort(arr);
-        tracker.stop();
-        System.out.println("Sorted array: " + Arrays.toString(arr));
-        tracker.printReport();
+        int[] originalArray = {64, 34, 25, 12, 22, 11, 90, 42, 5};
+        for (GapSequence seq : GapSequence.values()) {
+            System.out.println("\nTesting " + seq + " gap sequence:");
+            int[] arr = Arrays.copyOf(originalArray, originalArray.length);
+            ShellSort shellSort = new ShellSort(seq);
+            PerformanceTracker tracker = shellSort.getTracker();
+            tracker.start();
+            shellSort.sort(arr);
+            tracker.stop();
+            System.out.println("Sorted array: " + Arrays.toString(arr));
+            tracker.printReport();
+        }
     }
 }
